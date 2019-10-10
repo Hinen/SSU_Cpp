ae50fa24957afbf6b44ebd57e1e007df333bd142
Hinen <qkrqldtjqj@naver.com>
2019-10-10 20:02:48 +0900

    Week6 Manager Setting

--- Sources/Main.cpp
+++ Sources/Main.cpp
@@ -5,12 +5,29 @@
 using namespace std;
 
 //
+void CreateWeek4Manager();
+void CreateWeek6Manager();
+
+//
 int main() {
+	//CreateWeek4Manager();
+	CreateWeek6Manager();
+
+	return 1;
+}
+
+void CreateWeek4Manager() {
 	Week4Manager *week4Manager = new Week4Manager();
 
 	week4Manager->Main();
 
 	delete week4Manager;
+}
 
-	return 1;
+void CreateWeek6Manager() {
+	Week6Manager *week6Manager = new Week6Manager();
+
+	week6Manager->Main();
+
+	delete week6Manager;
 }
