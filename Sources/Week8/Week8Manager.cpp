df1a0166894f6b1c20922b8f61575810c5151d2f
Hinen <qkrqldtjqj@naver.com>
2019-10-24 19:53:00 +0900

    Init Week8Manager

---
+++ Sources/Week8/Week8Manager.cpp
@@ -0,0 +1,16 @@
+#include "Week8Manager.h"
+
+Week8Manager::Week8Manager() : WeekManagerBase(8)
+{
+	// DO NOTHING
+}
+
+Week8Manager::~Week8Manager()
+{
+	// DO NOTHING
+}
+
+void Week8Manager::Main()
+{
+	WeekManagerBase::Main();
+}
