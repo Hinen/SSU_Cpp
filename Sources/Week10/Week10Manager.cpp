53a36565ca0e1468480d28bd1b9291742dfc2862
Hinen <qkrqldtjqj@naver.com>
2019-11-07 19:57:21 +0900

    Init Week10Manager

---
+++ Sources/Week10/Week10Manager.cpp
@@ -0,0 +1,16 @@
+#include "Week10Manager.h"
+
+Week10Manager::Week10Manager() : WeekManagerBase(10)
+{
+	// DO NOTHING
+}
+
+Week10Manager::~Week10Manager()
+{
+	// DO NOTHING
+}
+
+void Week10Manager::Main()
+{
+	WeekManagerBase::Main();
+}
---
+++ Sources/Week10/Week10Manager.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "../WeekManagerBase.h"
+
+class Week10Manager : public WeekManagerBase
+{
+public:
+	Week10Manager();
+	~Week10Manager();
+
+public:
+	void Main();
+};
+
