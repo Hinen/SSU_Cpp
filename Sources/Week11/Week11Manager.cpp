633a89d58594eaa42db940eb2588acf3126c6d68
Hinen <qkrqldtjqj@naver.com>
2019-11-14 20:00:00 +0900

    Init Week11Manager

---
+++ Sources/Week11/Week11Manager.cpp
@@ -0,0 +1,16 @@
+#include "Week11Manager.h"
+
+Week11Manager::Week11Manager() : WeekManagerBase(11)
+{
+	// DO NOTHING
+}
+
+Week11Manager::~Week11Manager()
+{
+	// DO NOTHING
+}
+
+void Week11Manager::Main()
+{
+	WeekManagerBase::Main();
+}
---
+++ Sources/Week11/Week11Manager.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "../WeekManagerBase.h"
+
+class Week11Manager : public WeekManagerBase
+{
+public:
+	Week11Manager();
+	~Week11Manager();
+
+public:
+	void Main();
+};
