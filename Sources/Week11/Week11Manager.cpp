36f53a6783e0ab3ae5916febf90c54509115cee1
Hinen <qkrqldtjqj@naver.com>
2019-11-14 20:21:40 +0900

    Human Class Test

--- Sources/Week11/Week11Manager.cpp
+++ Sources/Week11/Week11Manager.cpp
@@ -13,4 +13,20 @@ Week11Manager::~Week11Manager()
 void Week11Manager::Main()
 {
 	WeekManagerBase::Main();
+
+	HumanClassTest();
+}
+
+void Week11Manager::HumanClassTest()
+{
+	_human = new Human();
+	_kgiYoon = new KgiYoon();
+
+	_human->kill1();
+
+	_kgiYoon->kill1();
+	_kgiYoon->kill2();
+
+	delete _kgiYoon;
+	delete _human;
 }
--- Sources/Week11/Week11Manager.h
+++ Sources/Week11/Week11Manager.h
@@ -7,6 +7,32 @@ public:
 	Week11Manager();
 	~Week11Manager();
 
+protected:
+	class Human
+	{
+	public:
+		void kill1()
+		{
+			std::cout << "Human kill 1" << std::endl;
+		}
+	};
+
+	class KgiYoon : public Human
+	{
+	public:
+		void kill2()
+		{
+			std::cout << "KgiYoon kill 2" << std::endl;
+		}
+	};
+
 public:
 	void Main();
+
+private:
+	Human *_human;
+	KgiYoon *_kgiYoon;
+
+private:
+	void HumanClassTest();
 };
