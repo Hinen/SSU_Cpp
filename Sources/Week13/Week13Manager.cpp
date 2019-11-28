06f6d0a5ab328f2b3c7765315cf1d1c50a1edf8a
Hinen <qkrqldtjqj@naver.com>
2019-11-28 21:11:32 +0900

    Strategy Pattern Base

--- Sources/Week13/Week13Manager.cpp
+++ Sources/Week13/Week13Manager.cpp
@@ -13,4 +13,21 @@ Week13Manager::~Week13Manager()
 void Week13Manager::Main()
 {
 	WeekManagerBase::Main();
+
+	StrategyPattern();
+}
+
+void Week13Manager::StrategyPattern()
+{
+	TeaGunV *teaGunV = new TeaGunV("TeaGunV");
+	Atom *atom = new Atom("Atom");
+
+	teaGunV->Attack();
+	teaGunV->Move();
+
+	atom->Attack();
+	atom->Move();
+
+	delete teaGunV;
+	delete atom;
 }
--- Sources/Week13/Week13Manager.h
+++ Sources/Week13/Week13Manager.h
@@ -7,6 +7,56 @@ public:
 	Week13Manager();
 	~Week13Manager();
 
+private:
+	class Robot
+	{
+	private:
+		std::string _name;
+
+	public:
+		Robot(std::string name)
+		{
+			_name = name;
+		}
+
+	public:
+		std::string GetName()
+		{
+			return _name;
+		}
+
+		virtual void Attack()
+		{
+			std::cout << "Robot Attack!" << std::endl;
+		}
+
+		virtual void Move()
+		{
+			std::cout << "Robot Move!" << std::endl;
+		}
+	};
+
+	class TeaGunV : public Robot
+	{
+	public:
+		TeaGunV(std::string name) : Robot(name)
+		{
+			// DO NOTHING
+		}
+	};
+
+	class Atom : public Robot
+	{
+	public:
+		Atom(std::string name) : Robot(name)
+		{
+			// DO NOTHING
+		}
+	};
+
 public:
 	virtual void Main() override;
+
+private:
+	void StrategyPattern();
 };
